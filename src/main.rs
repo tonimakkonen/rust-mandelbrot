9ac63fdbbf100f8919cc87f71c5eb2650a98e6f8
tonimakkonen <toni.a.makkonen@gmail.com>
2021-04-18 16:52:36 +0300

    learning macros

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
 extern crate gl;
 extern crate sdl2;
 
+use std::ffi::{CStr, CString};
 mod tmgl;
+
+macro_rules! file_name_and_content {
+    ($a:tt)=>{
+        ($a, include_str!($a))
+    };
+
+}
 
 fn main() {
     let sdl = sdl2::init().unwrap();
@@ -16,6 +24,10 @@ fn main() {
     let _gl_context = window.gl_create_context().unwrap();
     let _gl =
         gl::load_with(|s| video_subsystem.gl_get_proc_address(s) as *const std::os::raw::c_void);
+
+    let a = file_name_and_content!("triangle.frag");
+
+    tmgl::shader_from_file(("triangle.frag", include_str!("triangle.frag")));
 
     let mut event_pump = sdl.event_pump().unwrap();
     'main: loop {
--- src/tmgl.rs
+++ src/tmgl.rs
@@ -1,8 +1,75 @@
+#![macro_use]
+
 extern crate gl;
+
+use std::ffi::{CStr, CString};
 
 pub fn clear(r: f32, g: f32, b: f32, a: f32) {
     unsafe {
         gl::ClearColor(r, g, b, a);
         gl::Clear(gl::COLOR_BUFFER_BIT);
     }
+}
+
+//////////////////////
+// Creating shaders //
+//////////////////////
+
+/// Load a vertex or a fragment shader from a specified file.
+pub fn shader_from_file(name_and_content: (&str, &str)) {
+    let kind: gl::types::GLenum;
+    let name = name_and_content.0;
+    if name.ends_with(".vert") {
+        kind = gl::VERTEX_SHADER;
+    } else if name.ends_with(".frag") {
+        kind = gl::FRAGMENT_SHADER;
+    } else {
+        panic!("Unknown shader type: {}", name);
+    }
+    println!("{}", name_and_content.1);
+    //let res = shader_from_source(&CString::new(file_content).unwrap(), kind);
+}
+
+fn shader_from_source(source: &CStr, kind: gl::types::GLenum) -> Result<gl::types::GLuint, String> {
+    let id = unsafe { gl::CreateShader(kind) };
+    unsafe {
+        gl::ShaderSource(id, 1, &source.as_ptr(), std::ptr::null());
+        gl::CompileShader(id);
+    }
+
+    let mut success: gl::types::GLint = 1;
+    unsafe {
+        gl::GetShaderiv(id, gl::COMPILE_STATUS, &mut success);
+    }
+
+    if success == 0 {
+        let mut len: gl::types::GLint = 0;
+        unsafe {
+            gl::GetShaderiv(id, gl::INFO_LOG_LENGTH, &mut len);
+        }
+
+        let error = create_whitespace_cstring_with_len(len as usize);
+
+        unsafe {
+            gl::GetShaderInfoLog(
+                id,
+                len,
+                std::ptr::null_mut(),
+                error.as_ptr() as *mut gl::types::GLchar,
+            );
+        }
+
+        return Err(error.to_string_lossy().into_owned());
+    }
+
+    Ok(id)
+}
+
+fn create_whitespace_cstring_with_len(len: usize) -> CString {
+    // allocate buffer of correct size
+    let mut buffer: Vec<u8> = Vec::with_capacity(len + 1);
+    // fill it with len spaces
+    buffer.extend([b' '].iter().cycle().take(len));
+    // convert buffer to CString
+    unsafe { CString::from_vec_unchecked(buffer) }
 }
